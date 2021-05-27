fe3805a2b524fa06458a099b4c48fea93bbb4aa1
icarusgk <icarus.gk@gmail.com>
2021-05-26 23:48:59 -0600

    first commit

---
+++ gitrep.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo 'Pushing to a new Github repository...'
+echo 'Currently in: '
+pwd
+
+# If file doesn't exist
+if [[ ! -f README.md ]]
+then
+    echo '# angular-material' >> README.md
+fi
+
+git init
+
+# Add all or especified files
+if [[ $2 -eq 'a' ]]
+then
+    git add *
+else
+    for item in "$@";
+    do
+        git add item
+    done
+fi
+
+# Commit
+git commit -m "first commit"
+
+# Change branch
+git branch -M main
+
+# Add the remote repository
+git remote add origin https://github.com/icarusgk/"$1".git
+
+# Push to the remote repository
+git push -u origin main
